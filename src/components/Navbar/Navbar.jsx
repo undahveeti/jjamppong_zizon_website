1b7f4f6d9c9b042e2a292cc6ef2e5da80b6af73a
Andy Tran <undahvt@gmail.com>
2024-09-04 16:48:21 -0700

    adjusting navbar for tablet / phones

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -21,8 +21,18 @@ const Navbar = () => (
     <div className = "app__navbar-login">
       <a href="#login" className = "p__opensans"> Log In / Register </a>
       <div/>
-        <a href="/" className = "p__opensans"> Book Table </a>
+         <a href="/" className = "p__opensans"> Book Table </a>
       
+    </div>
+    <div className = "app__navbar-smallscreen">
+      <GiHamburgerMenu color ="#fff" fontSize = {27} onClick ={() => {}}/>
+      <ul className = "app__navbar-smallscreen-links"> 
+        <li className = "p__opensans"><a href="#home">Home</a></li>
+        <li className = "p__opensans"><a href="#about">About</a></li>
+        <li className = "p__opensans"><a href="#menu">Menu</a></li>
+        <li className = "p__opensans"><a href="#awards">Awards</a></li>
+        <li className = "p__opensans"><a href="#contact">Contact</a></li>
+      </ul>
     </div>
   </nav>
 );
