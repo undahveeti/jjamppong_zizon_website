bd59eec47645764f37278d7f7efd2778fb848df0
Andy Tran <undahvt@gmail.com>
2024-09-04 18:28:21 -0700

    about us section with history

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
-
+import {images} from '../../constants';
 import './AboutUs.css';
 
+
+
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className = "app__aboutus app__bg flex__center section__padding" id="about">
+    <div className = "app__aboutus-overlay flex__center">
+      <img src = {images.G} alt = "g letter" />
+    </div>
+    <div className = "app__aboutus-content flex__center">
+      <div className = "app__aboutus-content_about">
+        <h1 className = "headtext__cormorant">About Us</h1>
+        <img src = {images.spoon} alt="about_spoon" className = "spoon__img"/>
+        <p className = "p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button type = "button" className = "custom__button">Know More</button>
+      </div>
+    </div>
+    <div className = "app__aboutus-content_knife flex__center">
+      <img src = {images.knife} alt = "about_knife"/>
+    </div>
+    <div className = "app__aboutus-content flex__center">
+      <div className = "app__aboutus-content_history">
+        <h1 className = "headtext__cormorant">Our History</h1>
+        <img src = {images.spoon} alt="about_spoon" className = "spoon__img"/>
+        <p className = "p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button type = "button" className = "custom__button">Know More</button>
+      </div>
+    </div>
+
   </div>
 );
 
