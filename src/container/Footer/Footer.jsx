8038963d0b6f24bfa80957027e05aafead8f04d3
Andy Tran <undahvt@gmail.com>
2024-09-04 23:52:07 -0700

    implemented full footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,13 +1,46 @@
 import React from 'react';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 
+import { FooterOverlay, Newsletter } from '../../components';
+import { images } from '../../constants';
 import './Footer.css';
-import {FooterOverlay, Newsletter} from '../../components';
-import {FiFacebook, FiTwitter, FiInstagram} from 'react-icons/fi';
+
 const Footer = () => (
-  <div className = "app__footer section__padding">
-    <Newsletter/>
+  <div className="app__footer section__padding" id="login">
     <FooterOverlay />
-    
+    <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">3446 Wilshire Blvd, Los Angeles, CA 90010</p>
+        <p className="p__opensans">+1 999-999-9999</p>
+        <p className="p__opensans">+1 999-999-9999</p>
+      </div>
+
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="footer_logo" />
+        <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
+        <img src={images.spoon} alt="spoon__img" style={{ marginTop: 15 }} />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        
+        <p className="p__opensans">Monday-Sunday:</p>
+        <p className="p__opensans">010:00 am - 9:00 pm</p>
+      </div>
+    </div>
+
+    <div className="footer__copyright">
+      <p className="p__opensans">2024 Zizon Jjampong. All Rights reserved.</p>
+    </div>
+
   </div>
 );
 
