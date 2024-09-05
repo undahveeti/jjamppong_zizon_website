dbe802aae416219bde6c003d9636f35e84afa81b
Andy Tran <undahvt@gmail.com>
2024-09-04 21:04:23 -0700

    special menu barebones

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -2,9 +2,40 @@ import React from 'react';
 
 import './SpecialMenu.css';
 
+import {images, data} from '../../constants';
+
+import { SubHeading, MenuItem } from '../../components';
+
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className = "app__specialMenu flex__center section__padding" id = "menu">
+    <div className = "app__specialMenu-title">
+      <SubHeading title="Menu that fits your palatte"/>
+      <h1 className = "headtext__cormorant">Today's Special</h1>
+    </div>
+    <div className = "app__specialMenu-menu">
+      <div className = "app__specialMenu-menu_wine flex__center">
+        <p className = "app_specialMenu_menu_headings">Wine & Beer</p>
+        <div className = "app__specialMenu-menu_items">
+          {data.wines.map((wine, index) => (
+            <p>{wine.title}</p>
+          ))}
+        </div>
+      </div>
+      <div className = "app__specialMenu-menu_img">
+          <img src = {images.menu} alt = "menu_img"/>
+      </div>
+      <div className = "app__specialMenu-menu_cocktails flex__center">
+        <p className = "app_specialMenu_menu_headings">Wine & Beer</p>
+        <div className = "app__specialMenu-menu_items">
+          {data.cocktails.map((cocktail, index) => (
+            <p>{cocktail.title}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+    <div style={{marginTop: '15px'}}>
+      <button type = "button" className = "custom__button">View More</button>
+    </div>
   </div>
 );
 
