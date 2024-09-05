b4b70d1d381cdce944c6d91a4228c648301a8bc9
Andy Tran <undahvt@gmail.com>
2024-09-04 22:59:21 -0700

    find us page implemented

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import {images} from '../../constants';
 
+import { SubHeading } from '../../components';
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className = "app__bg app__wrapper section__padding" id="contact">
+    <div className = "app__wrapper_info">
+      <SubHeading title = "Contact" />
+      <h1 className = "headtext__cormorant" style = {{ marginBottom: '3rem'}}>Find Us</h1>
+      <div className = "app__wrapper-content">
+        <p className = "p__opensans">3446 Wilshire Blvd, Los Angeles, CA 90010</p>
+        <p className = "p__cormorant" style ={{color : '#DCCA87', margin: '2rem 0'}}>Opening Hours</p>
+        <p className = "p__opensans">Mon - Sun: 10:30am - 9:00pm </p>
+
+      </div>
+      <button className = "custom__button" style = {{marginTop: '2rem'}}>Visit Us</button>
+    </div>
+    <div className = "app__wrapper_img">
+      <img src = {images.findus} alt = "findus"/>
+    </div>
   </div>
 );
 
