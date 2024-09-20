75a02fa9091e7407684caa2493f131a3f214f7e6
Andy Tran <undahvt@gmail.com>
2024-09-19 17:18:47 -0700

    contact page and axios for cleaner http request

---
+++ src/container/Contact/Contact.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+import './Contact.css';
+
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    name: "",
+    message: "",
+    phoneNumber: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Send data to the backend API for processing
+      await axios.post("http://localhost:5000/send-email", formData);
+
+      // Clear form after successful submission
+      setFormData({
+        email: "",
+        name: "",
+        message: "",
+        phoneNumber: "",
+      });
+      alert("Message sent successfully!");
+    } catch (error) {
+      console.error("Error sending message:", error);
+      alert("There was an error sending your message.");
+    }
+  };
+
+  return (
+    <div className="contact-form">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Phone Number:
+          <input
+            type="text"
+            name="phoneNumber"
+            value={formData.phoneNumber}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Message:
+          <textarea
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
